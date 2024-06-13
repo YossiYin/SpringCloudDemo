76f5cec8e1ba236e6211fb794bc70ef649bb35da
Horace <youseeyhx@gmail.com>
2024-06-14 01:45:23 +0800

    修复bug

--- service-client/src/main/java/com/yen/service/BaiduFeignClient.java
+++ service-client/src/main/java/com/yen/service/BaiduFeignClient.java
@@ -1,11 +1,7 @@
 package com.yen.service;
 
-import com.yen.dto.baidu.TranslateRequest;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
